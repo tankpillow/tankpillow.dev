59337cdc547715ff3b5177918cf1301fd9e38968
msmith-codes <info@msmith.codes>
2024-12-12 13:09:37 -0500

    Added C++ guide

---
+++ src/app/resources/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function ResourcesPage()
+{
+    return (
+        <main>
+            <h1>/* Resources */</h1>
+            <ol>
+                <li>
+                    <Link href="/resources/cpp-0">Setting up C++</Link>
+                </li>
+            </ol>
+        </main>
+    );
+}
